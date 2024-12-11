8ad2bb009492597c97cf7a4a5fee346e134c37b8
tanjo <tanjo@users.noreply.github.com>
2024-12-11 17:25:42 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import SlackEmoji from './components/SlackEmoji.vue'
+
+export default {
+  components: {
+    HelloWorld,
+    SlackEmoji
+  },
+  data() {
+    return {
+      currentComponent: 'HelloWorld'
+    };
+  },
+};
+</script>
+
+<template>
+  <button v-if="currentComponent !== 'HelloWorld'" @click="currentComponent = 'HelloWorld'">
+    HelloWorld
+  </button>
+  <button v-if="currentComponent !== 'SlackEmoji'" @click="currentComponent = 'SlackEmoji'">
+    SlackEmoji
+  </button>
+  <KeepAlive>
+    <component v-bind:is="currentComponent" />
+  </KeepAlive>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/SlackEmoji.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+const font = ref<string>("Reggae One")
+
+function update() {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const ctx = canvas?.getContext('2d');
+    if (!ctx) return;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = '128px ' + font.value;
+
+    const textarea = document.getElementById('text') as HTMLTextAreaElement;
+    var lines = textarea.value.split('\n');
+
+    for (var i = 0; i<lines.length; i++) {
+        var line = lines[i] as string;
+
+        let metrics = ctx.measureText(line);
+        var x = (128 - metrics.actualBoundingBoxRight + metrics.actualBoundingBoxLeft) / 2;
+        var y = (128 - metrics.actualBoundingBoxDescent + metrics.actualBoundingBoxAscent) / 2;
+
+        console.log(x, y);
+
+        var lineheight = 128;
+
+        ctx.fillText(lines[i], x, y + (i * lineheight) );
+    }
+}
+</script>
+
+<template>
+    <h1>Slack Emoji</h1>
+    <button v-if="font !== 'Reggae One'" @click="font = 'Reggae One' ; update()">Reggae One</button>
+    <button v-if="font !== 'Yusei Magic'" @click="font = 'Yusei Magic' ; update()">Yusei Magic</button>
+    <div class="card">
+        <textarea id="text" @input="update"></textarea>
+    </div>
+    <div class="card">
+        <canvas id="canvas" width="128" height="128"></canvas>
+    </div>
+    <div class="card">
+        <p>Count: {{ count }}</p>
+    </div>
+</template>
+
+<style lang="css" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Reggae+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Reggae+One&family=Yusei+Magic&display=swap');
+canvas {
+    border: 1px solid black;
+}
+</style>
